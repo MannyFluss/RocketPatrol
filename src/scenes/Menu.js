0ad39155a85795a3047d8f5e03a49fc5a0c17892
Manny-OnGit <manny.fluss@gmail.com>
2022-04-09 13:51:08 -0700

    play.js made

---
+++ src/scenes/Menu.js
@@ -0,0 +1,18 @@
+class Menu extends Phaser.Scene 
+{
+
+    constructor(){
+        super("menuScene");//used to identify the scene (called insisde Phaser.scene)
+    }
+    create()
+    {
+        this.add.text(20,20, "Rocket Patrol Play");
+        this.scene.start("playScene");
+    }
+}
+
+
+// init() prepares any data for the scene
+// preload() prepares any assets we’ll need for the scene
+// create() adds objects to the scene
+// update() is a loop that runs continuously and allows us to update game objects
